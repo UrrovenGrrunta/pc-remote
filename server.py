5efd7396bf07c1ad94f81c3c627d302b6b4fdf4d
UrrovenGrrunta <77235483+UrrovenGrrunta@users.noreply.github.com>
2026-08-17 17:09:30 +0100

    Add HTTP server module

---
+++ server.py
@@ -0,0 +1,124 @@
+import os
+import launchers
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+APPS = {
+    "cs2": 730,
+    "gd": 322170,
+    "TBOI": 250900,
+    "ets_2": 227300,
+    "phasmo": 739630,
+    "ADOFAI": 977950,
+    "vcd": 246900,
+    "cloverpit": 3314790,
+    "starrupture": 1631270,
+    "satisfactory": 526870,
+    "creepy_support": 3685900,
+    "passport_blyat": 239030,
+    "graveyard_keeper": 599140,
+    "everlasting_summer": 331470,
+    "vcd_shadow_warrior": 255520,
+    "deep_rock_galactic": 548430,
+    "vcd_santas_rampage": 265210,
+}
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        match self.path:
+            case "/":
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+
+                with open(
+                    "static/index.html",
+                    "r",
+                    encoding="utf-8",
+                ) as file:
+                    html = file.read()
+                    self.wfile.write(html.encode("utf-8"))
+
+            case "/script.js":
+                self.send_response(200)
+                self.send_header(
+                    "Content-type",
+                    "text/javascript",
+                )
+                self.end_headers()
+
+                with open(
+                    "static/script.js",
+                    "r",
+                    encoding="utf-8",
+                ) as file:
+                    js = file.read()
+                    self.wfile.write(js.encode("utf-8"))
+
+            case "/style.css":
+                self.send_response(200)
+                self.send_header("Content-type", "text/css")
+                self.end_headers()
+
+                with open(
+                    "static/style.css",
+                    "r",
+                    encoding="utf-8",
+                ) as file:
+                    css = file.read()
+                    self.wfile.write(css.encode("utf-8"))
+
+            case path if path.startswith("/images/"):
+                image_name = path.removeprefix("/images/")
+                image_path = os.path.join(
+                    "static",
+                    "images",
+                    image_name,
+                )
+
+                if not os.path.isfile(image_path):
+                    self.send_response(404)
+                    self.end_headers()
+                    return
+
+                self.send_response(200)
+                self.send_header("Content-type", "image/jpeg")
+                self.end_headers()
+
+                with open(image_path, "rb") as file:
+                    self.wfile.write(file.read())
+
+            case "/launch/gr7":
+                launchers.launch_gr7()
+                self.send_response(200)
+                self.end_headers()
+
+            case "/launch/osu":
+                launchers.launch_osu()
+                self.send_response(200)
+                self.end_headers()
+
+            case "/launch/ubuntu":
+                launchers.launch_ubuntu()
+                self.send_response(200)
+                self.end_headers()
+
+            case path if path.startswith("/launch/"):
+                app_name = path.removeprefix("/launch/")
+                app_id = APPS[app_name]
+                launchers.launch_app(app_id)
+                self.send_response(200)
+                self.end_headers()
+
+            case _:
+                self.send_response(404)
+                self.end_headers()
+
+
+def create_server():
+    host = "100.85.81.84"
+    port = 8000
+
+    return HTTPServer((host, port), RequestHandler)
